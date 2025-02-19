2d161f1c7f2e4805d70edb2368a44474a95ad1c0
Luca Alessandri <alessandri.luca91@gmail.com>
2025-02-19 15:22:13 +0100

    Update and rename 0_generateLibInfo.sh to generateLibInfo.sh

--- HTGTS/generateLibInfo.sh
+++ HTGTS/generateLibInfo.sh
@@ -2,18 +2,19 @@
 
 # Function to display help message
 show_help() {
-    echo "Usage: $0 <xml_file_path>"
+    echo "Usage: $0 <xml_file_path> <configType>"
     echo
     echo "This script runs a Docker container for the htgts_pipeline_lts with the specified XML file."
     echo "It processes the XML file to generate libseqInfo.txt and libseqInfo2.txt within the same directory."
     echo
     echo "Arguments:"
-    echo "  xml_file_path   Path to the XML file to process."
+    echo "  xml_file_path     Path to the XML (Excel) file to process."
+    echo "  configType        Configuration type required by the script."
 }
 
-# Check if at least one argument is provided
-if [ $# -ne 1 ]; then
-    echo "Error: Missing XML file path."
+# Check if exactly two arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Error: Missing arguments."
     show_help
     exit 1
 fi
@@ -25,6 +26,7 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 fi
 
 xmlFile="$1"
+configType="$2"
 data_folder=$(dirname "$xmlFile")
 data_folder_name=$(basename "$xmlFile")
 
@@ -34,8 +36,15 @@ if [ ! -f "$xmlFile" ]; then
     exit 1
 fi
 
-# Run the Docker container
-docker run -v "${data_folder}:/Data" repbioinfo/htgts_pipeline_lts python3 /Algorithm/sample_sheetTolibInfo.py "/Data/$data_folder_name" /Data/libseqInfo.txt /Data/libseqInfo2.txt
+# Prepare the Docker command
+docker_command="docker run -v \"${data_folder}:/Data\" repbioinfo/htgts_pipeline_lts_v16 python3 /Algorithm/sample_sheetTolibInfo.py /Data/$data_folder_name /Data/libseqInfo.txt /Data/libseqInfo2.txt $configType"
+
+# Echo the command to be executed
+echo "Running the following command:"
+echo $docker_command
+
+# Execute the Docker command
+eval $docker_command
 
 if [ $? -eq 0 ]; then
     echo "The process has completed successfully."
